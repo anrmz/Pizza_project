a1dc5705e6626b28da368e376f84ece65c4d922e
anrmz <anouarmezgualli17@gmail.com>
2024-06-29 22:18:31 +0100

    message

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <Switch>
+        <Routes>
           <Route path="/" exact component={Home} />
           <Route path="/menu" exact component={Menu} />
           <Route path="/about" exact component={About} />
           <Route path="/contact" exact component={Contact} />
-        </Switch>
+        </Routes>
         <Footer />
       </Router>
     </div>
